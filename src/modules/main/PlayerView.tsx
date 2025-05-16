993689f767c24c09d0c3da18ec7e5f2e98af071c
spaenleh <spaenleh@gmail.com>
2025-05-16 12:03:34 +0200

    Add Player view code from live coding session

--- src/modules/main/PlayerView.tsx
+++ src/modules/main/PlayerView.tsx
@@ -1,22 +1,64 @@
-import { Box, Typography } from '@mui/material';
+import { ChangeEventHandler, useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
-import { useLocalContext } from '@graasp/apps-query-client';
+import { Close } from '@mui/icons-material';
+import {
+  Button,
+  IconButton,
+  Stack,
+  TextField,
+  Typography,
+} from '@mui/material';
 
-import { hooks } from '@/config/queryClient';
+import { hooks, mutations } from '@/config/queryClient';
 import { PLAYER_VIEW_CY } from '@/config/selectors';
 
 const PlayerView = (): JSX.Element => {
-  const { permission } = useLocalContext();
-  const { data: appContext } = hooks.useAppContext();
-  const members = appContext?.members;
+  const { t } = useTranslation();
+  const [answer, setAnswer] = useState('');
+  const { data: answerData } = hooks.useAppData<{ value: string }>({
+    type: 'answer',
+  });
+  const postAppData = mutations.usePostAppData();
+  const deleteAppData = mutations.useDeleteAppData();
+  const handleChange: ChangeEventHandler<
+    HTMLInputElement | HTMLTextAreaElement
+  > = (event) => {
+    const newValue = event.target.value;
+    setAnswer(newValue);
+    // eslint-disable-next-line no-console
+    console.log(newValue);
+  };
+
+  useEffect(() => {
+    if (answerData) {
+      setAnswer(answerData[answerData.length - 1].data.value);
+    }
+  }, [answerData]);
+
+  const handleSubmit = (): void => {
+    postAppData.mutate({ type: 'answer', data: { value: answer } });
+  };
 
   return (
-    <div data-cy={PLAYER_VIEW_CY}>
-      Player as {permission}
-      <Box p={2}>
-        <Typography>Members</Typography>
-        <pre>{JSON.stringify(members, null, 2)}</pre>
-      </Box>
+    <div data-cy={PLAYER_VIEW_CY} style={{ paddingBottom: '100px' }}>
+      <Stack direction="row">
+        <Typography>{t('Hello')}</Typography>
+        <TextField value={answer} onChange={handleChange} />
+        <Button onClick={handleSubmit}>Submit</Button>
+      </Stack>
+      {answerData?.map(({ id, data }) => (
+        <Stack key={id} direction="row">
+          <Typography>{data.value}</Typography>
+          <IconButton
+            onClick={() => {
+              deleteAppData.mutate({ id });
+            }}
+          >
+            <Close />
+          </IconButton>
+        </Stack>
+      ))}
     </div>
   );
 };
